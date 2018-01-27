d9220b90404dd0930c4826e76eae194d009de4d4
Skytrave <travelwithyousky@gmail.com>
2018-01-27 10:39:20 +0800

    try

--- 2018.1.27.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# _* _ coding:utf-8 _*_
-
-f=open('C:/Users/q1284/Desktop/change file.txt','w')
-f.write('似的发射点发射点'*10000)
-f=open('C:/Users/q1284/Desktop/change file.txt','r+')
-print(f.read())
-f.close()
