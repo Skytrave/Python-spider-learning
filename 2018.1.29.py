2bb47434726adb5a88967cc742d7de1e6feaeed8
skytrave <travelwithyousky@gmail.com>
2018-01-29 23:12:42 +0800

    面向对象编程

---
+++ 2018.1.29.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _* _ coding:utf-8 _*_
+#'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+import requests
+headers={
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+}
+res=requests.get('https://www.taobao.com/',headers=headers)
+try:
+	print(res.text)
+except ConnectionError:
+	print('拒接连接')
