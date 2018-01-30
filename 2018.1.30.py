3ba9b893e552b4aea1aa55cf7bb73c4cb119b317
skytrave <travelwithyousky@gmail.com>
2018-01-30 23:24:30 +0800

    BeautifulSoup 库&信息提取

---
+++ 2018.1.30.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# _* _ coding:utf-8 _*_
+#'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+import requests
+from bs4 import BeautifulSoup
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'}
+res=requests.get('https://bj.xiaozhu.com/',headers=headers)
+soup=BeautifulSoup(res.text,'html.parser')#解析返回的结果
+#print(soup.prettify())
+prices=soup.select('#page_list > ul > li > div.result_btm_con.lodgeunitname > span.result_price > i')
+for price in prices:
+	print(price.get_text())
+	
