084e29192df9e621645e75f174cec01313f8fbfe
skytrave <travelwithyousky@gmail.com>
2018-01-31 23:19:50 +0800

    BeautifulSoup 库&信息提取

--- 2018.1.31.py
+++ 2018.1.31.py
@@ -10,9 +10,36 @@ headers={
 def judgment_sex(class_name):
 	if class_name ==['member_ico']:
 		return '男'
-	else：
+	else:
 		return '女'#定义判断性别函数
 def get_links(url):
 	wb_data=requests.get(url,headers=headers)
 	soup=BeautifulSoup(wb_data.text,'lxml')
-
+	links=soup.select('#page_list > ul > li > a') #获取多个详细页的链接
+	for link in links:
+		href=link.get("href")
+		get_info(href)#获取单个详细页链接
+def get_info(url):
+	wb_data=requests.get(url,headers=headers)
+	soup=BeautifulSoup(wb_data.text,'lxml')
+	titles=soup.select('div.pho_info > h4 > em')
+	addresses=soup.select('div.pho_info > p > span')
+	prices=soup.select('#pricePart > div.day_l > span')
+	imgs=soup.select('#floatRightBox > div.js_box.clearfix > div.member_pic > a > img')
+	names=soup.select('#floatRightBox > div.js_box.clearfix > div.w_240 > h6 > a')
+	sexs=soup.select('#floatRightBox > div.js_box.clearfix > div.member_pic > div')
+	for title,address,price,img,name,sex in zip(titles,addresses,prices,imgs,names,sexs):
+		data={
+			'title':title.get_text().strip(),
+			'address':address.get_text().strip(),
+			'price':price.get_text(),
+			'img': img.get("src"),#src 图像文件的url
+			'name': name.get_text(),
+			'sex': judgment_sex(sex.get("class"))
+		}#获取信息并通过字典的信息打印
+	print(data)
+if __name__=='__main__':#程序主入口
+	urls=['https://bj.xiaozhu.com/search-duanzufang-p{}-0/'.format(number) for number in range(1,4)]
+	for single_url in urls:
+		get_links(single_url)#循环调用get_links()函数
+		time.sleep(2)#睡眠2秒
